b26094bd3a6ed2a56af9fda1cfd841f28b29f906
Erwing Hernandez <birc89@hotmail.com>
2025-06-21 15:20:40 -0600

    otros cambios

--- controllers/recomendaciones.controller.js
+++ controllers/recomendaciones.controller.js
@@ -1,9 +1,9 @@
-const Recomendacion = require("../models/Recomendaciones");
+const Recomendaciones = require("../models/Recomendaciones");
 
 // Obtener todos los registros de recomendaciones
 const getRecomendacion = async (req, res) => {
     try {
-        const recomendacion = await Recomendacion.find()
+        const recomendacion = await Recomendaciones.find()
         res.json(recomendacion);
     } catch (err) {
         res.status(500).json({ error: "Error al obtener control de software" });
@@ -13,7 +13,7 @@ const getRecomendacion = async (req, res) => {
 // Crear nuevo recomendaciones
 const createRecomendacion = async (req, res) => {
     try {
-        const nuevo = new Recomendacion(req.body);
+        const nuevo = new Recomendaciones(req.body);
         await nuevo.save();
         res.status(201).json(nuevo);
     } catch (err) {
@@ -24,7 +24,7 @@ const createRecomendacion = async (req, res) => {
 // Actualizar recomendaciones
 const updateRecomendacion = async (req, res) => {
     try {
-        const updated = await Recomendacion.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        const updated = await Recomendaciones.findByIdAndUpdate(req.params.id, req.body, { new: true });
         res.json(updated);
     } catch (err) {
         res.status(400).json({ error: "Error al actualizar registro de software" });
@@ -34,7 +34,7 @@ const updateRecomendacion = async (req, res) => {
 // Eliminar recomendaciones
 const deleteRecomendacion = async (req, res) => {
     try {
-        await Recomendacion.findByIdAndDelete(req.params.id);
+        await Recomendaciones.findByIdAndDelete(req.params.id);
         res.json({ mensaje: "Registro de software eliminado correctamente" });
     } catch (err) {
         res.status(500).json({ error: "Error al eliminar registro de software" });
