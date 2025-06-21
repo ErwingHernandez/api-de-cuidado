8c512afd58568aea2410c1fd48e2949da7f5561b
Erwing Hernandez <birc89@hotmail.com>
2025-06-21 15:01:37 -0600

    arreglado

--- routes/nino.routes.js
+++ routes/nino.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const controller = require("../controllers/nino.controller");
 
 router.get("/", controller.getNino);
-//router.get("/:id", controller.getNinoPorUsuario);
+router.get("/porusuario/:id", controller.getNinoPorUsuario);
 router.post("/create", controller.createNino);
 router.put("/:id", controller.updateNino);
 router.delete("/:id", controller.deleteNino);
